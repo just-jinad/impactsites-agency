705d182d658ec7136a4ca1b822c80e7d58658b2d
Jinad Tope Isaac <jinadtope66@gmail.com>
2025-07-03 22:22:14 +0100

    Initial commit – ImpactSites MVP

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import "./css/style.css";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+export const metadata = {
+  title: "Impactsites Agency",
+  description: "A website to help Charity organisations and other Foundations, have an online presence, giving them the needed visibilty.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body
+        className={`${inter.variable} bg-gray-50 font-inter tracking-tight text-gray-900 antialiased`}
+      >
+        <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
